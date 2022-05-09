d25482ac2bb2759eec39edee00bace4dfa1e144f
Katarzyna_Piotrowska <kasiapiotrowska08@gmail.com>
2022-05-09 22:34:37 +0200

    The BeautifulSoup approach- initial version3

--- BeautifulSoup/rossmann.py
+++ BeautifulSoup/rossmann.py
@@ -2,17 +2,16 @@
 #Reading product 1456/6393.
 #https://www.rossmann.pl/Produkt/Skora-wlosy-i-paznokcie/Altapharma-suplement-diety-biotyna-300-µg-40-szt,395129,9016 #
 # UnicodeEncodeError: 'ascii' codec can't encode character '\xb5' in position 76: ordinal not in range(128)
-from cgi    import test
-from fileinput import filename
-from typing import Type
+from unicodedata import category
 from urllib import request
-from bs4    import BeautifulSoup as BS
+from bs4 import BeautifulSoup as BS
 import pandas as pd
 import re
 from urllib.error import HTTPError
 from urllib.error import URLError
 import time
 import os
+import urllib.parse
 
 #################
 ### Functions ###
@@ -30,10 +29,27 @@ def getdata(url):
     except URLError as e:
         print("The server could not be found!")
         return 0
+    except UnicodeEncodeError as e:
+        try:
+            urlNew = urllib.parse.urlsplit(url)
+            urlNew = list(urlNew)
+            urlNew[2] = urllib.parse.quote(urlNew[2],safe='/,')
+            urlNew = urllib.parse.urlunsplit(urlNew)
+            html = request.urlopen(urlNew)
+            soup = BS(html.read(), 'html.parser')
+            return soup
+        except:
+            print("'ascii' codec can't encode character in the link: \n {}".format(url))
+            return 0
     else:
         soup = BS(html.read(), 'html.parser')
         return soup
 
+def safe_str(obj):
+    try: return str(obj)
+    except UnicodeEncodeError:
+        return obj.encode('ascii', 'ignore').decode('ascii')
+    return ""
 
 
 def loadLinksToProductFromPage(bs):
@@ -50,10 +66,11 @@ def loadLinksToProductFromPage(bs):
     for bsProduct in bsProducts:
         try:
             dProduct = {
-                'link':mainUrl + str(bsProduct.find('a',class_ = 'tile-product__name')['href']),
+                'link':mainUrl + safe_str(bsProduct.find('a',class_ = 'tile-product__name')['href']),
+                'regularPrice': ''
             }
             try:
-                dProduct['regularPrice'] = bsProduct.find(class_ = 'tile-product__old-price').text,
+                dProduct['regularPrice'] = bsProduct.find(class_ = 'tile-product__old-price').text
             except:
                 dProduct['regularPrice'] = "None"
             try:
@@ -64,7 +81,6 @@ def loadLinksToProductFromPage(bs):
             lProducts.append(dProduct)
         except:
             print("!!!!ERROR!!!! cannot load the link to the product. It is skipped.")
-        
     return(lProducts)
 
 
@@ -76,50 +92,71 @@ def getInfoAboutProduct(dProduct):
     The value accepted by the function is a dictionary containing
     as one of its values a link to the product page.
     '''
-    soup = getdata(dProduct['link']) # Download the page for the product 
-    Product['Name'] = soup.find('div',class_ = 'product-info__name').h1.text
-    Product['image'] = soup.find('div',class_ = 'product-img').img['src']
-    Product['shortDescription'] = soup.find('div',class_ = 'product-info__name').h2.text
-    Product['longDescription'] = soup.find('div',class_ = 'product__desctiption').find(class_ = 'accordion').div.text
-    Product['regularPrice'] = dProduct['regularPrice'][0]
-    Product['promoPrice'] = dProduct['promoPrice'] 
 
+    Prod = {}
+    soup = getdata(dProduct['link']) # Download the page for the product 
+    if soup == 0:
+        return 0
+    try:
+        Prod['name'] = soup.find('div',class_ = 'product-info__name').h1.text
+    except: Prod['name'] ="None"
+    
+    try:
+        Prod['image'] = 'https:' + str(soup.find('div',class_ = 'product-img').img['src'])
+    except: Prod['image'] ="None"
+
+    try:
+        Prod['shortDescription'] = soup.find('div',class_ = 'product-info__name').h2.text
+    except: Prod['shortDescription'] = "None"
+    
+    try:
+        Prod['longDescription'] = soup.find('div',class_ = 'product__desctiption').find(class_ = 'accordion').div.text
+    except: Prod['longDescription'] = "None"
+    
+    Prod['regularPrice'] = dProduct['regularPrice']
+    Prod['promoPrice'] = dProduct['promoPrice'] 
+    Prod['link'] = dProduct['link'] 
     # scrape additional info about product
     
     # If you can't find the word "NOT AVAILABLE ONLINE" on the page, 
     # it means that the product is available to order online, otherwise it is not available.
     if  soup.find_all(text='NIEDOSTĘPNY ONLINE'):  
-        Product['availability']  = "NIEDOSTĘPNY ONLINE"
+        Prod['availability']  = "NIEDOSTĘPNY ONLINE"
     else:
-        Product['availability']  = "DOSTĘPNY ONLINE"
+        Prod['availability']  = "DOSTĘPNY ONLINE"
     
     # save the information about the categories in which the product is placed
-    categories = soup.find_all(class_= 'breadcrumb-item')
-    categoriesList = [category.a.text for category in categories[1:-1]]
-    Product['categories'] = "/".join(categoriesList)
-    
+    try:
+        categories = soup.find_all(class_= 'breadcrumb-item')
+        categoriesList = [category.a.text for category in categories[1:-1]]
+        Prod['categories'] = "/".join(categoriesList)
+        Prod['countCategories'] = len(categoriesList)
+    except:
+        Prod['categories'] = "None"
+        Prod['countCategories'] = "None"
+
     # collect info about the review
     try:
         review = soup.find(class_= 'product-info__rate d-flex py-2').find_all('span')
         if review:
-            Product['rate'] = review[0]['data-rate']
-            Product['numberOfReviews'] = review[1].text
+            Prod['rate'] = review[0]['data-rate']
+            Prod['numberOfReviews'] = review[1].text
         else:
-            Product['rate'] = "None"
-            Product['numberOfReviews'] = "None"
+            Prod['rate'] = "None"
+            Prod['numberOfReviews'] = "None"
     except:
-            Product['rate'] = "None"
-            Product['numberOfReviews'] = "None"
+            Prod['rate'] = "None"
+            Prod['numberOfReviews'] = "None"
     
     try:      
             if soup.find(class_= 'product-info__tags').text:
-                Product['gender'] = soup.find(class_= 'product-info__tags').text
+                Prod['gender'] = soup.find(class_= 'product-info__tags').text
             else:
-                Product['gender'] = "None"
+                Prod['gender'] = "None"
     except:
-            Product['gender'] = "None"
-        
-    return Product
+            Prod['gender'] = "None"
+    
+    return Prod
 
 ####################
 ### Main program ###
@@ -128,10 +165,12 @@ def getInfoAboutProduct(dProduct):
 # get the start time
 st = time.time()
 
-fileName = "200Products.csv"
+fileName = "AllProducts.csv"
+ifSave = True
 directory = os.getcwd()
+ProdNotLoaded = []
 
-numberOfProductsMax = 100 # # the maximum number of products for which the program should scrape data
+numberOfProductsMax = 100000000 # # the maximum number of products for which the program should scrape data
 mainUrl = 'https://www.rossmann.pl'
 bs = getdata(mainUrl)
 
@@ -165,22 +204,83 @@ N = len(lProducts)
 Products  = []
 i = 1
 # scrape and save all information about all products linked in the lProducts list
+
+#lProducts[0]['link'] = 'https://www.rossmann.pl/Produkt/Ozdoby-do-wlosow/For-Your-Beauty-Ozdoby-do-wlosow-#FYB-Gumki-do-wlosow-czarne-MICHAELA-15-szt-1,219919,8678'
 for Product in lProducts:
     print('Reading product {}/{}.\n {} \n'.format(i, N, Product['link']))
-    Products.append(getInfoAboutProduct(Product))
+    dProduct = getInfoAboutProduct(Product)
+    if dProduct == 0:
+        ProdNotLoaded.append(Product['link'])
+    else:
+        Products.append(dProduct)
     i += 1
 
+Log = []
+print('\n\n---------------------')
+print('Status of the program:')
+print('---------------------')
 # save data as an external csv file 
-df = pd.DataFrame(Products)
-df.to_csv(fileName)
-pathToFile = os.path.join(directory,fileName)
-print("Scraped data was saved here: {}".format(pathToFile))
+if ifSave:
+    if Products:
+        df = pd.DataFrame(Products)
+#        numberOfCatCols = max( df['countCategories'])
+#        if df.countCategories < numberOfCatCols:
+#            for i in range(numberOfCatCols - df.countCategories):
+#                df.countCategories =  df.countCategories + '/'
+
+#        print(df.categories.str.split(pat='/',expand=True))
+#        df[['Cat1', 'Cat2', 'Cat3']] = df.categories.str.split(pat='/',expand=True)
+        df.to_csv(fileName)
+        pathToFile = os.path.join(directory,fileName)
+        msg  = "Scraped data was saved here: {}".format(pathToFile)
+        Log.append(msg)
+        print()
+    else:
+        msg = "No data to save as an external file"
+        Log.append(msg)
+        print(msg)
+else:
+    msg = 'Data was scraped but do not saved anywhere'
+    Log.append(msg)
+    print(msg)
+
+msg = 'Number od scraped products: {}'.format(len(Products))
+Log.append(msg)
+print(msg)
 
 # get the end time
 et = time.time()
 
 # get the execution time
 elapsed_time = et - st
-print('Execution time:', round(elapsed_time,4), 'seconds')
-print('Execution time:', round(elapsed_time/60,4), 'minutes')
+msg = 'Execution time: {} seconds.'.format(round(elapsed_time,4))
+Log.append(msg)
+print(msg)
+
+msg = 'Execution time: {} seconds.'.format(round(elapsed_time/60,4))
+Log.append(msg)
+print(msg)
+
+print('\n\n------------------------------------')
+msg = 'Products that have not been scraped:'
+Log.append(msg)
+print(msg)
+print('------------------------------------')
+
+if ProdNotLoaded:
+    for prod in ProdNotLoaded:
+        msg = '    * {}'.format(prod)
+        Log.append(msg)
+        print(msg)
+else:
+    msg = 'None'
+    Log.append(msg)
+    print(msg)
+
+fileName = 'log.txt'
+with open(fileName, mode="w") as f:  # also, tried mode="rb"
+    for line in Log:
+        f.write("%s\n" % line)
+
+print('\nLog file is saved here: ' , os.path.join(directory,fileName))
 
