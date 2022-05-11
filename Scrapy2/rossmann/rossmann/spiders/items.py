a0242c6288fff99f640765a81a4e133c17ea8a0f
Katarzyna_Piotrowska <kasiapiotrowska08@gmail.com>
2022-05-11 11:49:30 +0200

    Numb of reviews and old/regular prices

---
+++ Scrapy2/rossmann/rossmann/spiders/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.item import Item, Field
+class Rossmann(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+#    Name = Field()
+#    Size = Field()
+#    Link = Field()
+#    Availability = Field()
+#    rate = Field()     
+    #numberOfReviews = Field()
+    price_regular = Field()
+    price_promo = Field()
+    pass
